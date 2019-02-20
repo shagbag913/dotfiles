d01b7b1ef0a1cf5f08f79a6178acf4d875f24f70
shagbag913 <sh4gbag913@gmail.com>
2019-02-19 21:55:56 -0500

    batteryprompt: add battery warning and use subprocess
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- .bin/batteryprompt.py
+++ .bin/batteryprompt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import psutil
-import os
+import sys
+import subprocess
 
 def secs2hours(secs):
     mm, ss = divmod(secs, 60)
@@ -10,8 +11,21 @@ def secs2hours(secs):
     else:
         return ("%dmin" % mm)
 
+def sendnotif(msg, icon):
+    subprocess.call(['notify-send.sh', '--icon=' + icon, '--app-name=Battery', msg])
+
 battery = psutil.sensors_battery()
 battery_pct = round(battery.percent)
+
+# battery low warning
+try:
+    if sys.argv[1] == 'low':
+        icon = '/usr/share/icons/Numix/48/status/gtk-dialog-warning-panel.svg'
+        sendnotif('Warning: battery low.\nCharge: {}'.format(battery_pct), icon)
+        quit()
+except IndexError:
+    None # do nothing, no argument was specified
+
 icon = '/usr/share/icons/Numix/48/devices/'
 
 if battery_pct < 20:
@@ -32,4 +46,4 @@ if battery_state == True:
 else:
     remain = "{} remaining".format(secs2hours(battery_time))
 
-os.system('notify-send.sh --app-name=Battery --icon={}.svg "{}% charged\n{}"'.format(icon, battery_pct, remain))
+sendnotif("{}% charged\n{}".format(battery_pct, remain), icon)
