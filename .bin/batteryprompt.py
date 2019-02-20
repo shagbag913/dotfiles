a3d418ae9559aa8beeea1097b798f1ba0ad9f1e4
shagbag913 <sh4gbag913@gmail.com>
2019-02-19 21:28:35 -0500

    batteryprompt: cleanup a bit
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- .bin/batteryprompt.py
+++ .bin/batteryprompt.py
@@ -11,33 +11,25 @@ def secs2hours(secs):
         return ("%dmin" % mm)
 
 battery = psutil.sensors_battery()
+battery_pct = round(battery.percent)
+icon = '/usr/share/icons/Numix/48/devices/'
+
+if battery_pct < 20:
+    icon += 'battery-caution'
+elif battery_pct < 40:
+    icon += 'battery-low'
+elif battery_pct < 80:
+    icon += 'battery-good'
+else:
+    icon += 'battery-full'
+
 battery_state = battery.power_plugged
 battery_time = battery.secsleft
-battery_pct = battery.percent
 
 if battery_state == True:
-    if battery_pct < 20:
-        icon = 'battery-caution-charging'
-    elif battery_pct < 40:
-        icon = 'battery-low-charging'
-    elif battery_pct < 80:
-        icon = 'battery-good-charging'
-    else:
-        icon = 'battery-full-charging'
-
-    remain = "Charging"
+    remain = 'Charging'
+    icon += '-charging'
 else:
-    if battery_pct < 20:
-        icon = 'battery-caution'
-    elif battery_pct < 40:
-        icon = 'battery-low'
-    elif battery_pct < 80:
-        icon = 'battery-good'
-    else:
-        icon = 'battery-full'
+    remain = "{} remaining".format(secs2hours(battery_time))
 
-    remain = "%s remaining" % secs2hours(battery_time)
-
-
-icon = '/usr/share/icons/Numix/48/devices/%s.svg' % icon
-os.system('notify-send.sh --app-name=Battery --icon=%s "%i%% charged\n%s"' % (icon, round(battery_pct), remain))
+os.system('notify-send.sh --app-name=Battery --icon={}.svg "{}% charged\n{}"'.format(icon, battery_pct, remain))
