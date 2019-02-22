4ea960f751d816d0baf277a7b2b433679f826a07
shagbag913 <sh4gbag913@gmail.com>
2019-02-21 20:55:28 -0500

    batteryprompt: don't use stupid lockfile
    dont be dumb, use variable lul
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- .bin/batteryprompt_wrap.py
+++ .bin/batteryprompt_wrap.py
@@ -6,6 +6,7 @@ import sys
 
 battery = psutil.sensors_battery()
 plugged = battery.power_plugged
+showlow = True
 
 while True:
     battery = psutil.sensors_battery()
@@ -24,8 +25,8 @@ while True:
     except IndexError:
         None # no argument
 
-    if battery_pct <= 15 and battery_state != True and not os.path.exists('/tmp/batterylock'):
+    if battery_pct <= 15 and battery_state != True and showlow == True:
         batteryprompt.lowprompt(battery_pct)
-        open('/tmp/batterylock', 'a').close()
-    elif (battery_pct > 16 or battery_state == True) and os.path.exists('/tmp/batterylock'):
-        os.remove('/tmp/batterylock')
+        showlow = False
+    elif (battery_pct > 16 or battery_state == True) and showlow == False:
+        showlow = True
