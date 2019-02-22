5693222a76e123b1142a9a1ef6c1dc96b05bfcd0
shagbag913 <sh4gbag913@gmail.com>
2019-02-21 20:22:14 -0500

    batteryprompt: don't define variables twice
    just include everything in the while loop instead.
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- .bin/batteryprompt_wrap.py
+++ .bin/batteryprompt_wrap.py
@@ -4,23 +4,19 @@ import os
 import psutil
 import sys
 
-battery = psutil.sensors_battery()
-battery_pct = round(battery.percent)
-battery_state = battery.power_plugged
-battery_time = battery.secsleft
-
-try:
-    if sys.argv[1] == 'click':
-        batteryprompt.statprompt(battery_pct, battery_state, battery_time)
-        quit()
-except IndexError:
-    None
-
 while True:
     battery = psutil.sensors_battery()
     battery_pct = round(battery.percent)
     battery_state = battery.power_plugged
     battery_time = battery.secsleft
+
+    try:
+        if sys.argv[1] == 'click':
+            batteryprompt.statprompt(battery_pct, battery_state, battery_time)
+            quit()
+    except IndexError:
+        None # no argument
+
     if battery_pct <= 15 and battery_state != True and not os.path.exists('/tmp/batterylock'):
         batteryprompt.lowprompt(battery_pct)
         open('/tmp/batterylock', 'a').close()
