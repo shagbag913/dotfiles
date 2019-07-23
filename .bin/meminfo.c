25ab96c8d1a9370fac6ebfefdaacaa045d1d3e32
shagbag913 <sh4gbag913@gmail.com>
2019-07-22 22:58:59 -0400

    meminfo: code cleanup
    - use goto's to unclutter main()
    - (unsigned long) var; -> (unsigned long)var;
    - strcmp(a, b) == 0  ->  !strcmp(a, b)
    - move function brackets to the next line
    - cleanup format division by using a for loop
    
    TODO: dynamically allocate memory for return string to support more RAM
    
    Signed-off-by: shagbag913 <sh4gbag913@gmail.com>

--- .bin/meminfo.c
+++ .bin/meminfo.c
@@ -17,50 +17,43 @@ struct meminfo {
 
 struct meminfo mem_stats();
 
-int main(int argc, char *argv[]) {
-	int byte_type = 0, percentage_total = 0, quiet = 0;
+int main(int argc, char *argv[])
+{
+	int byte_type = 0, percentage_total = 0, quiet = 0, tmp;
 	float return_mem = 0;
-	char return_mem_str[30];
+	char return_mem_str[30]; //TODO: dynammically allocate
 	struct meminfo mi = mem_stats();
 
-	if (argc < 2) {
-		printf("ERR: Not enough arguments\n");
-		return 1;
-	}
+	if (argc < 2)
+		goto not_enough_args;
 
-	for (int iter = 0; iter < argc; iter++) {
-		if (strcmp(argv[iter], "--percentage-total") == 0 || strcmp(argv[iter], "-p") == 0)
+	for (tmp = 0; tmp < argc; tmp++) {
+		if (!strcmp(argv[tmp], "--percentage-total") || !strcmp(argv[tmp], "-p"))
 			percentage_total = 1;
 
-		if ((strcmp(argv[iter], "--quiet") == 0 || strcmp(argv[iter], "-q") == 0) && !percentage_total)
+		if ((!strcmp(argv[tmp], "--quiet") || !strcmp(argv[tmp], "-q")) && !percentage_total)
 			quiet = 1;
 
-		if ((strcmp(argv[iter], "--kilobytes") == 0 || strcmp(argv[iter], "-k") == 0) && !percentage_total)
+		if ((!strcmp(argv[tmp], "--kilobytes") || !strcmp(argv[tmp], "-k")) && !percentage_total)
 			byte_type = 1;
-		else if ((strcmp(argv[iter], "--megabytes") == 0 || strcmp(argv[iter], "-m") == 0) && !percentage_total)
+		else if ((!strcmp(argv[tmp], "--megabytes") || !strcmp(argv[tmp], "-m")) && !percentage_total)
 			byte_type = 2;
-		else if ((strcmp(argv[iter], "--gigabytes") == 0 || strcmp(argv[iter], "-g") == 0) && !percentage_total)
+		else if ((!strcmp(argv[tmp], "--gigabytes") || !strcmp(argv[tmp], "-g")) && !percentage_total)
 			byte_type = 3;
 
-		if (strcmp(argv[iter], "--free") == 0 || strcmp(argv[iter], "-f") == 0)
+		if (!strcmp(argv[tmp], "--free") || !strcmp(argv[tmp], "-f"))
 			return_mem = mi.free;
-		else if (strcmp(argv[iter], "--used") == 0 || strcmp(argv[iter], "-u") == 0)
+		else if (!strcmp(argv[tmp], "--used") || !strcmp(argv[tmp], "-u"))
 			return_mem = mi.used;
-		else if (strcmp(argv[iter], "--total") == 0 || strcmp(argv[iter], "-t") == 0)
+		else if (!strcmp(argv[tmp], "--total") || !strcmp(argv[tmp], "-t"))
 			return_mem = mi.total;
 	}
 
-	if (!return_mem) {
-		printf("ERR: No memory return type specified\n");
-		return 1;
-	}
+	if (!return_mem)
+		goto no_mem_type;
 
-	if (byte_type == 1)
-		return_mem = return_mem / 1000;
-	else if (byte_type == 2)
-		return_mem = return_mem / 1000 / 1000;
-	else if (byte_type == 3)
-		return_mem = return_mem / 1000 / 1000 / 1000;
+	for (tmp = 0; tmp < byte_type; tmp++)
+		return_mem /= 1000;
 
 	if (quiet) {
 		printf("%0.0f\n", return_mem);
@@ -69,24 +62,34 @@ int main(int argc, char *argv[]) {
 	} else {
 		switch (byte_type) {
 			case 0:
-				sprintf(return_mem_str, "%0.2f B", return_mem);
+				sprintf(return_mem_str, "%0.0fB", return_mem);
 				break;
 			case 1:
-				sprintf(return_mem_str, "%0.2f kB", return_mem);
+				sprintf(return_mem_str, "%0.0fkB", return_mem);
 				break;
 			case 2:
-				sprintf(return_mem_str, "%0.2f MB", return_mem);
+				sprintf(return_mem_str, "%0.0fMB", return_mem);
 				break;
 			case 3:
-				sprintf(return_mem_str, "%0.2f GB", return_mem);
+				sprintf(return_mem_str, "%0.2fGB", return_mem);
 				break;
 		}
 
 		printf("%s\n", return_mem_str);
 	}
+
+	return 0;
+
+not_enough_args:
+	printf("ERR: no arguments specified.\n");
+	return 1;
+no_mem_type:
+	printf("ERR: no memory return type specified.\n");
+	return 1;
 }
 
-struct meminfo mem_stats() {
+struct meminfo mem_stats()
+{
 	char file_content[20];
 	struct meminfo mi;
 	int loc = 0;
@@ -94,22 +97,22 @@ struct meminfo mem_stats() {
 	FILE *memfile = fopen("/proc/meminfo", "r");
 
 	while (fscanf(memfile, "%s", file_content) != EOF) {
-		if (strcmp(file_content, "MemTotal:") == 0) {
+		if (!strcmp(file_content, "MemTotal:")) {
 			loc = 1;
 			continue;
-		} else if (strcmp(file_content, "MemFree:") == 0) {
+		} else if (!strcmp(file_content, "MemFree:")) {
 			loc = 2;
 			continue;
-		} else if (strcmp(file_content, "Buffers:") == 0) {
+		} else if (!strcmp(file_content, "Buffers:")) {
 			loc = 3;
 			continue;
-		} else if (strcmp(file_content, "Cached:") == 0) {
+		} else if (!strcmp(file_content, "Cached:")) {
 			loc = 4;
 			continue;
-		} else if (strcmp(file_content, "Shmem:") == 0) {
+		} else if (!strcmp(file_content, "Shmem:")) {
 			loc = 5;
 			continue;
-		} else if (strcmp(file_content, "SReclaimable:") == 0) {
+		} else if (!strcmp(file_content, "SReclaimable:")) {
 			loc = 6;
 			continue;
 		}
@@ -117,17 +120,17 @@ struct meminfo mem_stats() {
 		// /proc/meminfo records values in kB by default, convert them to bytes.
 		switch (loc) {
 			case 1:
-				mi.total = (unsigned long) atoi(file_content) * 1000;
+				mi.total = (unsigned long)atoi(file_content) * 1000;
 			case 2:
-				mi.free = (unsigned long) atoi(file_content) * 1000;
+				mi.free = (unsigned long)atoi(file_content) * 1000;
 			case 3:
-				mi.buffers = (unsigned long) atoi(file_content) * 1000;
+				mi.buffers = (unsigned long)atoi(file_content) * 1000;
 			case 4:
-				mi.cached = (unsigned long) atoi(file_content) * 1000;
+				mi.cached = (unsigned long)atoi(file_content) * 1000;
 			case 5:
-				mi.shmem = (unsigned long) atoi(file_content) * 1000;
+				mi.shmem = (unsigned long)atoi(file_content) * 1000;
 			case 6:
-				mi.sreclaimable = (unsigned long) atoi(file_content) * 1000;
+				mi.sreclaimable = (unsigned long)atoi(file_content) * 1000;
 		}
 
 		loc = 0;
