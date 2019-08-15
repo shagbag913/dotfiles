824ba41d6f159c042ff737aa12da06e5c869a1ee
shagbag913 <sh4gbag913@gmail.com>
2019-08-14 20:54:24 -0400

    Revert "lemonbar_utils: fix get_pywal_color_value"
    This reverts commit 621080a0f0d28cb2bcf106b6c0d8999f92b75cd5.

--- .bin/lemonbar_utils.c
+++ .bin/lemonbar_utils.c
@@ -132,7 +132,8 @@ void *function_thread(void *function_args)
 char *get_pywal_color_value(int color_index, char *fallback_color)
 {
 	FILE *pywal_file;
-	char pywal_path[100], *line;
+	static char line[10];
+	char pywal_path[100];
 	const char *suffix_pywal_path = "/.cache/wal/colors";
 	int tmp = 0;
 
@@ -143,8 +144,6 @@ char *get_pywal_color_value(int color_index, char *fallback_color)
 
 	if (pywal_file == NULL)
 		return fallback_color;
-
-	line = malloc(strlen(fallback_color) > 6 ? 10 : 8);
 
 	while (fgets(line, sizeof(line), pywal_file) != NULL) {
 		if (tmp == color_index * 2)
