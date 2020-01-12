f812a3513da2e4611d8c97c64c19d33f1a835ef9
shagbag913 <sh4gbag913@gmail.com>
2020-01-12 18:05:26 -0500

    Add Golang based lemonbar config
    Change-Id: Idb2eb15fafad876a371e8c268ba2723f487f8055

---
+++ .bin/gobar.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+    "fmt"
+    "time"
+    "strconv"
+    "io/ioutil"
+    "os/exec"
+    "strings"
+)
+
+/* Printed bar strings */
+var timeString string
+var chargeString string
+var bspwmStatus string
+
+/* Other */
+var lastBspwmStatus []byte
+
+/* All goroutines must set this if their corresponding global variable is changed */
+var shouldMainPrint bool
+
+func main() {
+    /* Initialize goroutines */
+    go setTimeString()
+    go setChargeString()
+    go setBspwmStatus()
+
+    /* Main loop */
+    for {
+        if shouldMainPrint == false {
+            time.Sleep(100 * time.Millisecond)
+        }
+
+        printBuffer := ""
+
+        if bspwmStatus != "" {
+            printBuffer += "%{l}" + bspwmStatus
+        }
+
+        if timeString != "" {
+            printBuffer += "%{c}" + timeString
+        }
+
+        if chargeString != "" {
+            printBuffer += "%{r}" + chargeString
+        }
+
+        if printBuffer != "" {
+            printBuffer = "    " + printBuffer + "    "
+            fmt.Println(printBuffer)
+        }
+
+        shouldMainPrint = false
+        time.Sleep(100 * time.Millisecond)
+    }
+}
+
+func setTimeString() {
+    for {
+        tNow := time.Now()
+        minute, hour := tNow.Minute(), tNow.Hour()
+
+        if hour > 12 {
+            hour -= 12
+        }
+
+        newTimeString := strconv.Itoa(hour) + ":"
+
+        if minute < 10 {
+            newTimeString += "0" + strconv.Itoa(minute)
+        } else {
+            newTimeString += strconv.Itoa(minute)
+        }
+
+        if newTimeString != timeString {
+            timeString = newTimeString
+            shouldMainPrint = true
+        }
+
+        time.Sleep(10 * time.Second)
+    }
+}
+
+func setChargeString() {
+    for {
+        /* TODO: unhardcode */
+        charge, err := ioutil.ReadFile("/sys/class/power_supply/BAT0/capacity")
+
+        if err != nil {
+            /* DNE, break */
+            break
+        }
+
+        newChargeString := string(charge[:len(charge) - 1]) + "%"
+
+        if newChargeString != chargeString {
+            chargeString = newChargeString
+            shouldMainPrint = true
+        }
+
+        time.Sleep(10 * time.Second)
+    }
+}
+
+func testEqual(a, b []byte) bool {
+    if (a == nil) != (b == nil) {
+        return false
+    }
+
+    if len(a) != len(b) {
+        return false
+    }
+
+    for i := range a {
+        if a[i] != b[i] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func setBspwmStatus() {
+    for {
+        /* TODO: talk directly to socket */
+        socketStatus, _ := exec.Command("bspc", "wm", "--get-status").Output()
+
+        /* Don't continue if socketStatus hasn't changed */
+        if testEqual(socketStatus, lastBspwmStatus) == true {
+            time.Sleep(100 * time.Millisecond)
+            continue
+        }
+        lastBspwmStatus = socketStatus
+
+        socketStatusSplit := strings.Split(string(socketStatus), ":")
+        newBspwmStatus := ""
+
+        for i := 0; i < len(socketStatusSplit); i++ {
+            socketStatusEl := string(socketStatusSplit[i])
+            switch socketStatusEl[:1] {
+            case "F":
+                fallthrough
+            case "O":
+                newBspwmStatus += " %{+u}  " + strconv.Itoa(i) + "  %{-u} |"
+            case "o":
+                newBspwmStatus += "   " + strconv.Itoa(i) + "   |"
+            }
+        }
+
+        /* Remove ending spacer */
+        newBspwmStatus = newBspwmStatus[:len(newBspwmStatus)-1]
+
+        bspwmStatus = newBspwmStatus
+        shouldMainPrint = true
+        time.Sleep(100 * time.Millisecond)
+    }
+}
